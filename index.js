3681c0e32be88159b1b574652fc7ab7b60dfec2e
Victoria Olatunji <aboladegeovhykee@gmail.com>
2022-07-18 17:18:14 +0100

    second commit

--- index.js
+++ index.js
@@ -27,8 +27,20 @@ function playRound(playerSelection, computerSelection){
 
 function game(){
   for(let i = 0; i < 5; i++){
-      let input = prompt('Please enter your selection...');
-      console.log(playRound(input, computerPlay()));
+    let input = prompt('Please enter selection...'); 
+      input = input.toLowerCase();
+      if( input === 'rock' || input === 'paper' || input === 'scissors'){
+        console.log(`Round ${i+1} \n${playRound(input, computerPlay())}`);
+      }else{
+       console.log('Invalid selection,choose only rock paper scissors');
+       i--;
+      }
+    }
+    return '';
   }
-}
 console.log(game());
+
+
+
+
+
